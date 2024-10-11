857cce4ac9b64fa10d2bd0fdaf92192ac7ee66a9
Aearhart-Dominican <austin.earhart55@gmail.com>
2024-10-10 20:44:32 -0700

    add item and show item functions working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 import data from './data.js'
 
 const itemsContainer = document.querySelector('#items')
+const cart = []
 
 for (let i = 0; i < data.length; i += 1) {
 	// create a new div element and give it a class name
@@ -39,3 +40,24 @@ for (let i = 0; i < data.length; i += 1) {
 	newDiv.appendChild(button)
 
 }
+
+const addItemToCart = (id, price) => {
+    // Loop over cart items. 
+    for (let i = 0; i < cart.length; i += 1) {
+        // If we find a matching item increase the quantity
+        if (cart[i].id === id) {
+            cart[i].qty += 1
+            return // exit this function early
+        }
+    }
+    // If no matching items were found add a new item
+    cart.push({ id, price, qty: 1 })
+}
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.matches('.add-to-cart')) {
+        console.log(e.target)
+        addItemToCart(e.target.id, e.target.dataset.price)
+        console.log(cart) // Use console.log to test your work
+    }
+})
